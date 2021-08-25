8996709150ad3b80fdf7492740bd4ff182586bda
Tyago Teoi <tyagoyuji@gmail.com>
2021-08-25 19:17:56 -0300

    Add the QUIT button functionality
    When the user clicks to the button "QUIT" in the Menu scene,
    the application closes.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Change the current scene to the playable game scene
     public void PlayGame ()
     {
         // Loads the next level in the queue of scenes
         // Obs: the default scene for the Menu is 0, and for the playable game is 1
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    // Closes the game
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT FUNCTION ENTRY");
+        Application.Quit();
+    }
+
 }
