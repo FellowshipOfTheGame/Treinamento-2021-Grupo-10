985eb05fa377ee238f2a68b42078c4f5baf401e3
Tyago Teoi <tyagoyuji@gmail.com>
2021-08-25 18:35:52 -0300

    Add the 'PLAY' button functionality
    When the user clicks to the 'PLAY' button, the scene is changed
    to the playable game.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        // Loads the next level in the queue of scenes
+        // Obs: the default scene for the Menu is 0, and for the playable game is 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
