afe6c46a25fd711ed4626148a9913e3cce21b0e4
Tyago Teoi <tyagoyuji@gmail.com>
2021-08-26 17:11:45 -0300

    Add the Volume Setting in the Game Menu
    The user can change the game volume in the Game Main Menu
    adjusting the VolumeSlider.

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioMixer audioMixer;
 
-    // Update is called once per frame
-    void Update()
+    // Set the volume of the game
+    public void SetVolume(float volume)
     {
-        
+        // "volume" is the exposed script parameter for the Master Volume of the game
+        audioMixer.SetFloat("volume", volume);
     }
 }
