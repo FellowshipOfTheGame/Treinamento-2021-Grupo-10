9a8d050efdee0c25b9070c2f70a676fd956b52bb
Tyago Teoi <tyagoyuji@gmail.com>
2021-09-06 18:55:36 -0300

    Add the collectables for the game
    Set a HUD for the three collectables.
    
    All the scripts were set and it work and interact
    with the HUD of the game.
    
    The next step will be (maybe) add point for collect
    these collectables.

---
+++ Assets/Scripts/Scarab.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scarab : MonoBehaviour
+{
+    // Receive the HUD with pyramids, related to the collectables
+    public GameObject[] Pyramid; 
+
+    // Trigger when the player hit the colectable
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Search for the active Pyramid HUD
+        for (int i = 0; i < Pyramid.Length - 1; i++)
+        {
+            // If is active in the HUD, sets it false
+            // and put the next pyramid active
+            if (Pyramid[i].activeSelf)
+            {
+                Pyramid[i].SetActive(false);
+                Pyramid[i + 1].SetActive(true);
+                
+                break;
+            }
+        }
+
+        // Destroy the collectable for the scene
+        Destroy(gameObject);
+    }
+}
