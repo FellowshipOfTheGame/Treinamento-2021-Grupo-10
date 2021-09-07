b94f8184b860845add6de8e72d00d8948acb5ff9
Tyago Teoi <tyagoyuji@gmail.com>
2021-09-07 08:31:07 -0300

    Add the Game Over Menu
    The script and the functionality of the game over menu
    is working, with the basic buttons as "PLAY AGAIN",
    "MAIN MENU" and "QUIT GAME".
    
    The events that open the Game Over Menu have not
    been programed yet.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Load the Play scene
+    public void PlayAgain()
+    {
+        // Play the click sound
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Play");
+    }
+
+    // Load the Main Menu scene
+    public void LoadMenu()
+    {
+        // Play the click sound
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Quit the game
+    public void QuitGame()
+    {
+        // Play the click sound
+        FindObjectOfType<AudioManager>().Play("ButtonClick");
+
+        Debug.Log("Quit the game");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,12 +10,6 @@ public class PauseMenu : MonoBehaviour
     // Receive the PauseMenu game object
     public GameObject pauseMenuUI;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
