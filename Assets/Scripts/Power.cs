870fbdde86c2fd62ebf72aeecd0616ca337dd659
Tyago Teoi <tyagoyuji@gmail.com>
2021-09-10 21:20:50 -0300

    Add background to the game

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -7,6 +7,7 @@ public class Power : MonoBehaviour
     public enum Type { up, down };
     public Type type;
     public ChariotController chariotController;
+    public Animator animator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -22,8 +23,13 @@ public class Power : MonoBehaviour
                 // Play the powerup sound
                 FindObjectOfType<AudioManager>().Play("PowerUp");
 
+                // Set the HUD for the power up true
+                animator.SetBool("isPowerUpActive", true);
+
                 StartCoroutine(ChangeCharacterSpeed(1.5f, 1f));
 
+                // Set the HUD for the power up false
+                animator.SetBool("isPowerUpActive", false);
 
                 // Unpause the background music
                 FindObjectOfType<AudioManager>().Unpause("PlayTheme0");
@@ -38,8 +44,13 @@ public class Power : MonoBehaviour
                 // Play the powerdown sound
                 FindObjectOfType<AudioManager>().Play("PowerDown");
 
+                // Set the HUD for the power down true
+                animator.SetBool("isPowerDownActive", true);
+
                 StartCoroutine(ChangeCharacterSpeed(0.75f, 1f));
 
+                // Set the HUD for the power down false
+                animator.SetBool("isPowerDownActive", false);
 
                 // Unpause the background music
                 FindObjectOfType<AudioManager>().Unpause("PlayTheme0");
@@ -48,6 +59,7 @@ public class Power : MonoBehaviour
         }
 
         // Destroy the collectable for the scene
+        //gameObject.SetActive(false);
         Destroy(gameObject);
     }
 
@@ -57,6 +69,5 @@ public class Power : MonoBehaviour
         yield return new WaitForSeconds(time);
 
         chariotController.speed *= 1.0f / multiplier;
-
     }
 }
