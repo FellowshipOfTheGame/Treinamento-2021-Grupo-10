e335379d8dd04e378b7ca6d588a2bc9c9c35cbbb
Tyago Teoi <tyagoyuji@gmail.com>
2021-09-11 11:22:54 -0300

    Fix powerup and powerdown strange behavior

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -6,6 +6,7 @@ public class Power : MonoBehaviour
 {
     public enum Type { up, down };
     public Type type;
+    public float duration = 4f;
     public ChariotController chariotController;
     public Animator animator;
 
@@ -17,29 +18,18 @@ public class Power : MonoBehaviour
             case Type.up:
                 Debug.Log("Collected a power up");
 
-                // Pause the background music
-                FindObjectOfType<AudioManager>().Pause("PlayTheme0");
-
                 // Play the powerup sound
                 FindObjectOfType<AudioManager>().Play("PowerUp");
 
                 // Set the HUD for the power up true
                 animator.SetBool("isPowerUpActive", true);
 
-                StartCoroutine(ChangeCharacterSpeed(1.5f, 1f));
-
-                // Set the HUD for the power up false
-                animator.SetBool("isPowerUpActive", false);
-
-                // Unpause the background music
-                FindObjectOfType<AudioManager>().Unpause("PlayTheme0");
+                StartCoroutine (Pickup ( 1.5f, duration) );
 
                 break;
+
             case Type.down:
                 Debug.Log("Collected a power down");
-
-                // Pause the background music
-                FindObjectOfType<AudioManager>().Pause("PlayTheme0");
 
                 // Play the powerdown sound
                 FindObjectOfType<AudioManager>().Play("PowerDown");
@@ -47,27 +37,47 @@ public class Power : MonoBehaviour
                 // Set the HUD for the power down true
                 animator.SetBool("isPowerDownActive", true);
 
-                StartCoroutine(ChangeCharacterSpeed(0.75f, 1f));
-
-                // Set the HUD for the power down false
-                animator.SetBool("isPowerDownActive", false);
-
-                // Unpause the background music
-                FindObjectOfType<AudioManager>().Unpause("PlayTheme0");
+                StartCoroutine (Pickup (0.75f, duration) );
 
                 break;
         }
-
-        // Destroy the collectable for the scene
-        //gameObject.SetActive(false);
-        Destroy(gameObject);
     }
 
-    IEnumerator ChangeCharacterSpeed(float multiplier, float time)
+    IEnumerator Pickup(float multiplier, float time)
     {
+        // Pause the background music
+        FindObjectOfType<AudioManager>().Pause("PlayTheme0");
+
+        // Apply the power effect to the player
         chariotController.speed *= multiplier;
+
+        // Remove the image and collision of the powerup
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
         yield return new WaitForSeconds(time);
 
+        // "Remove" the power effect to the player
         chariotController.speed *= 1.0f / multiplier;
+
+        // Unpause the background music
+        FindObjectOfType<AudioManager>().Unpause("PlayTheme0");
+
+        // Get back to the normal animation
+        switch(type)
+        {
+            case Type.up:
+                // Set the HUD for the power up false
+                animator.SetBool("isPowerUpActive", false);
+                break;
+
+            case Type.down:
+                // Set the HUD for the power down false
+                animator.SetBool("isPowerDownActive", false);
+                break;
+        }
+
+        // Destroy the collectable in the scene
+        Destroy(gameObject);
     }
 }
