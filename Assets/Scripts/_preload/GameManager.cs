30003eb5322e72d6954364e1561146ca8b8108f1
Tyago Teoi <tyagoyuji@gmail.com>
2021-10-01 21:39:31 -0300

    Add __preload scene

---
+++ Assets/Scripts/_preload/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public GameObject GameOverMenu;
+    public GameObject VictoryMenu;
+    public GameObject HUD;
+    bool isGameEnded = false;
+
+    // Called when the player wins
+    public void WinGame()
+    {
+        //HUD.SetActive(false);
+        Debug.Log("VICTORY LOG");
+
+        // Stop the music
+        FindObjectOfType<AudioManager>().Stop("PlayTheme0");
+
+        // Play victory sound
+        FindObjectOfType<AudioManager>().Play("Victory");
+
+        // Freeze the game
+        Time.timeScale = 0f;
+
+        // Set the text of the points with the current point of the player
+        FindObjectOfType<VictoryMenu>().SetPoints();
+
+        // Open the victory menu
+        VictoryMenu.SetActive(true);
+    }
+
+    // Called when the player dies
+    public void EndGame()
+    {
+        if (!isGameEnded)
+        {
+            //HUD.SetActive(false);
+            isGameEnded = true;
+            Debug.Log("GAME OVER LOG");
+
+            // Play death sound
+            FindObjectOfType<AudioManager>().Play("Death");
+
+            // Stop the music
+            FindObjectOfType<AudioManager>().Stop("PlayTheme0");
+
+            // Add a delay for the player see his death
+
+            // Freeze the game
+            Time.timeScale = 0f;
+
+            // Open the game over menu
+            GameOverMenu.SetActive(true);
+        }
+    }
+}
