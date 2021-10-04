daed8170dcc41237519f207afa7003e324963154
Tyago Teoi <tyagoyuji@gmail.com>
2021-10-03 21:48:26 -0300

    Add a singleton for only the developers
    The object called 'OnlyForDevelopment' is now
    used to load the __preload scene from any scene.
    
    This object pass the name of the current scene
    for the _preload scene, using it to jump from the
    _preload scene to the current scene automaticly.

--- Assets/Scripts/_preload/App.cs
+++ Assets/Scripts/_preload/App.cs
@@ -7,7 +7,9 @@ public class App : MonoBehaviour
 {
     void Start()
     {
-        SceneManager.LoadScene("Menu");
+        // Uses the name of the scene selected using the DevPreload Singleton
+        string nextScene = FindObjectOfType<DevPreload>().scene.ToString();
+        Debug.Log(nextScene);
+        SceneManager.LoadScene(nextScene);
     }
-
 }
--- Assets/Scripts/_preload/DevPreload.cs
+++ Assets/Scripts/_preload/DevPreload.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// Used for access the preload scene from any scene 
+// Used for access the preload scene from any scene and other stuffs
+// This is a Singleton
 // obs: ONLY USED FOR DEVELOPMENT, NOT IMPLEMENTED FOR THE GAME
 public class DevPreload : MonoBehaviour
 {
+    public static DevPreload Instance { get; private set; }
+    public enum Type { Menu, Play };
+    public Type scene;
+
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        Debug.Log(scene);
+
         GameObject check = GameObject.Find("__app");
 
         if (check == null)
